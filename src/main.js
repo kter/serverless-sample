34ca85ece7c4f16747b43466b24eb87bca1a9386
Tomohiko Takahashi <takahashi@tomohiko.io>
2023-07-20 15:57:57 +0900

    PrimeVue導入

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 createApp(App).mount('#app')
